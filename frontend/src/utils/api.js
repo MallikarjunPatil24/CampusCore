b2e63be5d6875995a49a639a72abbcf853f5f053
Mallikarjun patil <mallikarjunpatil.tech@gmail.com>
2026-01-03 15:57:21 +0530

    Frontend: Pointing API to live Render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000' 
+    // Replace localhost with your actual Render backend URL
+    baseURL: 'https://campuscore-c4gs.onrender.com' 
 });
 
 API.interceptors.request.use((req) => {
